d74d4085ae2cc297ecf1d71268b9e19c4850c577
fabiocaettano <fabio.caettano74@gmail.com>
2023-01-03 23:01:20 +0000

    testes com kafka.js

--- src/infra/messaging/kafka/messaging.module.ts
+++ src/infra/messaging/kafka/messaging.module.ts
@@ -2,17 +2,13 @@ import { SendNotification } from '@application/use-cases/send-notification';
 import { DatabaseModule } from '@infra/database/database.module';
 import { Module } from '@nestjs/common';
 import { KafkaConsumerService } from './kafka-consumer.service';
-import { NotificationController } from '@infra/http/controller/notification-controller';
-import { ReadNotification } from '@application/use-cases/read-notifications';
-import { UnreadNotification } from '@application/use-cases/unread-notifications';
-import { CountRecipientNotifications } from '@application/use-cases/count-recipientid-notifications';
-import { GetRecipientNotifications } from '@application/use-cases/get-recipient-notifications';
-import { CancelNotification } from '@application/use-cases/cancel-notification';
+import { NotificationsController } from './controllers/notifictions.controller';
+
 
 @Module({
     imports: [DatabaseModule],
-    providers: [KafkaConsumerService, SendNotification, ReadNotification, UnreadNotification, CountRecipientNotifications,GetRecipientNotifications, CancelNotification],
-    controllers: [NotificationController],
+    providers: [KafkaConsumerService, SendNotification],
+    controllers: [NotificationsController],
 })
 
 export class MessagingModule {}
